aaa95f264b8538ca73b0a3ffb0680535b7501d1b
ahkim76 <ahkim9976@gmail.com>
2024-08-24 18:54:40 -0400

    Test

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,10 +1,14 @@
+import { NavLink } from "react-router-dom"
 import LogoImg from "../assets/Logo-NoBG.png"
 function Logo() {
     return (
-        <div className="text-center">
-            <img src={LogoImg} alt="HeelSync Logo" 
-            className="h-10 w-48"/>
-        </div>
+        <NavLink to="/dashboard" className="px-1">
+            <div className="text-center">
+                <img src={LogoImg} alt="HeelSync Logo" 
+                className="h-10 w-48"/>
+            </div>  
+        </NavLink>
+        
     )
 }
 
