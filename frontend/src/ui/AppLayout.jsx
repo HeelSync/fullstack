dc284b76c0a9fdcb836b0a1db3539999b4a35806
ahkim76 <ahkim9976@gmail.com>
2024-12-02 22:49:49 -0500

    Fix styles

--- frontend/src/ui/AppLayout.jsx
+++ frontend/src/ui/AppLayout.jsx
@@ -4,20 +4,17 @@ import Loader from "./Loader";
 import Sidebar from "./Sidebar";
 
 function AppLayout() {
-    return (
-        <div className="grid grid-cols-[18rem_1fr] grid-rows-[auto_1fr] h-screen">
-            
-            <Header />
-            <Sidebar />
-            <div className="overflow-scroll">
-                <main className="max-w-3x mx-auto">
-                    <Outlet />
-                </main>
-            </div>
-            
-
-        </div>
-    )
+  return (
+    <div className="grid h-screen grid-cols-[16rem_1fr] grid-rows-[auto_1fr]">
+      <Header />
+      <Sidebar />
+      <div className="overflow-scroll">
+        <main className="max-w-3x mx-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
--- frontend/src/ui/MainNav.jsx
+++ frontend/src/ui/MainNav.jsx
@@ -1,77 +1,114 @@
-import { NavLink, useLocation } from "react-router-dom"
-import {HiOutlineCog6Tooth, HiOutlineCalendar, HiOutlinePaperAirplane, HiOutlineShoppingCart, HiOutlineHome, HiOutlineLink, } from "react-icons/hi2"
+import { NavLink, useLocation } from "react-router-dom";
+import {
+  HiOutlineCog6Tooth,
+  HiOutlineCalendar,
+  HiOutlinePaperAirplane,
+  HiOutlineShoppingCart,
+  HiOutlineHome,
+  HiOutlineLink,
+} from "react-icons/hi2";
 function MainNav() {
-    const location = useLocation();
-    return (
-        <ul className="flex flex-col gap-2">
-            <li>
-            <NavLink 
-                to="/dashboard"
-                className={({ isActive }) =>
-                `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-                ${isActive ? 'text-blue-900 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-                }
-            >
-                <HiOutlineHome className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-brand-600' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                />
-                <span>Home</span>
-            </NavLink>
-            </li>
-
-            <li><NavLink to="/registration"
+  const location = useLocation();
+  return (
+    <ul className="flex flex-col gap-2">
+      <li>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-900" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+        >
+          <HiOutlineHome
             className={({ isActive }) =>
-                `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-                ${isActive ? 'text-blue-900 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-                }
-            >
-                <HiOutlineShoppingCart className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-brand-600' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                />
-            <span>Classes</span>
-            </NavLink></li>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-brand-600" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />
+          <span>Home</span>
+        </NavLink>
+      </li>
 
-            <li><NavLink to="/calendar"
+      <li>
+        <NavLink
+          to="/registration"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-900" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+        >
+          <HiOutlineShoppingCart
             className={({ isActive }) =>
-                `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-                ${isActive ? 'text-blue-900 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-                }>
-            <HiOutlineCalendar className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-brand-600' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                /><span>Calendar</span>
-            </NavLink></li>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-brand-600" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />
+          <span>Classes</span>
+        </NavLink>
+      </li>
 
-            <li><NavLink to="/links"
+      <li>
+        <NavLink
+          to="/calendar"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-900" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+        >
+          <HiOutlineCalendar
             className={({ isActive }) =>
-                `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-                ${isActive ? 'text-blue-900 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-                }>
-            <HiOutlineLink className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-brand-600' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                /> <span>Links</span>
-            </NavLink></li>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-brand-600" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />
+          <span>Calendar</span>
+        </NavLink>
+      </li>
 
-            <li><NavLink to="/studyabroadplanner"
+      <li>
+        <NavLink
+          to="/links"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-900" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+        >
+          <HiOutlineLink
             className={({ isActive }) =>
-                `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-                ${isActive ? 'text-blue-600 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-                }>
-            <HiOutlinePaperAirplane className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-blue-900' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                /><span>Study Abroad</span>
-            </NavLink></li>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-brand-600" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />{" "}
+          <span>Links</span>
+        </NavLink>
+      </li>
 
-            <li><NavLink to="/settings"
-           className={({ isActive }) =>
-            `flex items-center gap-4 text-lg font-medium py-3 px-6 transition-colors duration-300 rounded-sm
-            ${isActive ? 'text-blue-900 bg-gray-50' : 'text-better-white hover:text-blue-800 hover:bg-gray-50'}`
-            }s>
-            <HiOutlineCog6Tooth className={({ isActive }) => `w-8 h-8 transition-colors duration-300 
-                ${isActive ? 'text-brand-600' : 'text-gray-400 group-hover:text-brand-600 group-active:text-brand-600'}`} 
-                /> <span>Settings</span>
-            </NavLink></li>
-        </ul>
-    )
+      <li>
+        <NavLink
+          to="/studyabroadplanner"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-600" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+        >
+          <HiOutlinePaperAirplane
+            className={({ isActive }) =>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-blue-900" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />
+          <span>Abroad</span>
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) =>
+            `flex items-center gap-4 rounded-sm px-6 py-3 text-lg font-medium transition-colors duration-300 ${isActive ? "bg-gray-50 text-blue-900" : "text-better-white hover:bg-gray-50 hover:text-blue-800"}`
+          }
+          s
+        >
+          <HiOutlineCog6Tooth
+            className={({ isActive }) =>
+              `h-8 w-8 transition-colors duration-300 ${isActive ? "text-brand-600" : "group-hover:text-brand-600 group-active:text-brand-600 text-gray-400"}`
+            }
+          />{" "}
+          <span>Settings</span>
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
-export default MainNav
+export default MainNav;
